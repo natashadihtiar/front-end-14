afcbd2048d2e01aea8e0a779be08d0c3117397cd
natashadihtiar <47831860+natashadihtiar@users.noreply.github.com>
2020-03-30 21:21:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-magic-numbers */ 
 function sum (number) {
     let newNumber = 0;
     return function() {
-        let argument = arguments[0] ? arguments[0] : 0;
+        const argument = arguments[0] ? arguments[0] : 0;
         return newNumber += argument;
     };
 }
